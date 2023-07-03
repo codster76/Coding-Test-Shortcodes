aa447b3e9d7e82bb7cbb811126ab89270163e391
codster76 <codster761@gmail.com>
2023-07-03 17:12:56 +1000

    Implemented reversible randomisation and applied it to shortcode generation/decoding

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ Things to figure out:
   - Hexadecimals are promising (C8 2710 1F C 63), but it's still 11 characters long at most.
   - I could optimise a little and only track days in the year, but that would only save characters in base 10
   - Dates are the issue. I don't think there's a great way to compress dates without losing any info, so including a full date in the code is not too feasible.
-  - I think it's very reasonable to assume that the start date of a promotion is known and can be stored on internal systems.
-  - If we track days since the beginning of the promotion, we can fit up to 4095 days (>11 years) into 3 characters, which is likely enough.
+  - Knowing time since a start date would be ideal. I believe the 1st January 1970 default date should work for this.
 
   - Base 64 (200 10000 36500 64) -> (DI CcQ I6U +)
     - Convert all values to base64
     - Include a randomisation seed at the end
     - Keep a private key in the code
-    - Problem with only using base64 is that our possible values are far smaller than the possible number sets, generally causing
+    - Problem with only using base64 is that our possible values are far smaller than the possible number sets, generally causing 
     a large number of leading zeroes. Choosing smaller number systems would more evenly distribute our values across the number
     systems. It might also make it harder to crack.
     - I cannot feasibly go much over ~base 70 because you'd run out of common standard characters on an english keyboard
@@ -58,10 +57,7 @@ There are two parts to this problem:
 
 */
 
-// Figure out how to implement a linear congruential generator to map numbers
-// https://stackoverflow.com/questions/44981398/how-to-map-number-in-a-range-to-another-in-the-same-range-with-no-collisions
-
-// You could add more or less characters. These ones are mostly for demonstration purposes
+// You could add greater or fewer characters, like '[' or '.'. These ones are mostly chosen for ease of demonstration.
 const characterLookupTable = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
@@ -69,7 +65,7 @@ const characterLookupTable = [
     'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 
     'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
     'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
-    'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', "*", 
+    'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*', 
     '?'
 ];
 
@@ -80,33 +76,7 @@ const dateBase = characterLookupTable.length;
 const seedBase = characterLookupTable.length;
 
 // TODO: Modify this function
-// function generateShortCode(storeId, transactionId) {
-//     // Logic goes here
-
-//     const today = new Date();
-//     const date = Math.ceil(Math.abs(today - promotionStart) / (1000 * 60 * 60 * 24));
-
-//     const seedValue = Math.round(Math.random() * (63 - 1) + 1);
-
-//     // Possible values are a fairly small 
-//     const twoDigitNumberToAddAround = (Math.pow(64, 2)) / 2;
-//     const threeDigitNumberToAddAround = (Math.pow(64, 3)) / 2;
-    
-//     const randomisedArray = arrayRandomisation(characterLookupTable, seedValue);
-
-//     let base64StoreID = convertToAnyBase(shopBase, twoDigitNumberToAddAround + storeId, randomisedArray, 2);
-//     let base64TransactionID = convertToAnyBase(transactionBase, threeDigitNumberToAddAround + transactionId, randomisedArray, 3);
-//     let base64Date = convertToAnyBase(dateBase, threeDigitNumberToAddAround + date, randomisedArray, 3);
-//     let base64Seed = convertToAnyBase(seedBase, seedValue, characterLookupTable, 1);
-
-//     shortCode = base64StoreID + base64TransactionID + base64Date + base64Seed;
-    
-//     return shortCode;
-// }
-
 function generateShortCode(storeId, transactionId) {
-    // Logic goes here
-
     const today = new Date();
     const todayInDays = Math.ceil(today / (1000 * 60 * 60 * 24));
 
@@ -114,184 +84,111 @@ function generateShortCode(storeId, transactionId) {
     
     const randomisedArray = arrayRandomisation(characterLookupTable, seedValue);
 
-    let base64StoreID = convertToAnyBase(shopBase, storeId, randomisedArray, 2);
-    let base64TransactionID = convertToAnyBase(transactionBase, transactionId, randomisedArray, 3);
-    let base64Date = convertToAnyBase(dateBase, todayInDays, randomisedArray, 3);
-    let base64Seed = convertToAnyBase(seedBase, seedValue, characterLookupTable, 1);
+    const base64StoreID = convertToAnyBase(shopBase, storeId, randomisedArray, 2);
+    const base64TransactionID = convertToAnyBase(transactionBase, transactionId, randomisedArray, 3);
+    const base64Date = convertToAnyBase(dateBase, todayInDays, randomisedArray, 3);
+    const base64Seed = convertToAnyBase(seedBase, seedValue, characterLookupTable, 1);
 
-    shortCode = base64StoreID + base64TransactionID + base64Date + base64Seed;
+    // Shortcode is shuffled. This helps to avoid predictable repeat characters.
+    const unshuffledShortCodeAsArray = `${base64StoreID}${base64TransactionID}${base64Date}`.split('');
+    const shuffledShortCodeAsArray = arrayRandomisation(unshuffledShortCodeAsArray, seedValue);
+
+    const shortCode = shuffledShortCodeAsArray.join('') + base64Seed;
     
     return shortCode;
 }
 
 // TODO: Modify this function
-// function decodeShortCode(shortCode) {
-//     // Logic goes here
-//     const seedValue = convertAnyToBase10(seedBase, shortCode.charAt(shortCode.length - 1), characterLookupTable);
-
-//     const randomisedArray = arrayRandomisation(characterLookupTable, seedValue);
-
-//     const twoDigitNumberToAddAround = (Math.pow(64, 2)) / 2;
-//     const threeDigitNumberToAddAround = (Math.pow(64, 3)) / 2;
-
-//     let storeID = convertAnyToBase10(shopBase, shortCode.substring(0,2), randomisedArray) - twoDigitNumberToAddAround;
-//     let transactionID = convertAnyToBase10(transactionBase, shortCode.substring(2,5), randomisedArray) - threeDigitNumberToAddAround;
-//     let dateOfTransaction = convertAnyToBase10(dateBase, shortCode.substring(5,8), randomisedArray) - threeDigitNumberToAddAround;
-
-//     dateOfTransaction = dateOfTransaction * 24 * 60 * 60 * 1000;
-//     dateOfTransaction = +promotionStart + dateOfTransaction;
-
-//     return {
-//         storeId: storeID, // store id goes here,
-//         shopDate: new Date(dateOfTransaction), // the date the customer shopped,
-//         transactionId: transactionID, // transaction id goes here
-//     };
-// }
-
 function decodeShortCode(shortCode) {
     // Logic goes here
     const seedValue = convertAnyToBase10(seedBase, shortCode.charAt(shortCode.length - 1), characterLookupTable);
 
     const randomisedArray = arrayRandomisation(characterLookupTable, seedValue);
 
-    let storeID = convertAnyToBase10(shopBase, shortCode.substring(0,2), randomisedArray);
-    let transactionID = convertAnyToBase10(transactionBase, shortCode.substring(2,5), randomisedArray);
-    let dateOfTransaction = convertAnyToBase10(dateBase, shortCode.substring(5,8), randomisedArray);
+    const shortCodeWithoutSeed = shortCode.substring(0,8).split('');
+    const unshuffledShortCode = arrayReverseRandomisation(shortCodeWithoutSeed, seedValue).join('');
 
-    dateOfTransaction = dateOfTransaction * 24 * 60 * 60 * 1000;
+    let storeID = convertAnyToBase10(shopBase, unshuffledShortCode.substring(0,2), randomisedArray);
+    let transactionID = convertAnyToBase10(transactionBase, unshuffledShortCode.substring(2,5), randomisedArray);
+    let dateOfTransaction = convertAnyToBase10(dateBase, unshuffledShortCode.substring(5,8), randomisedArray);
+
+    // Date rounding seems to cause mismatches in dates, so 1 day needs to be subtracted
+    dateOfTransaction = (dateOfTransaction - 1) * 24 * 60 * 60 * 1000;
 
     return {
-        storeId: storeID, // store id goes here,
-        shopDate: new Date(dateOfTransaction), // the date the customer shopped,
-        transactionId: transactionID, // transaction id goes here
+        storeId: storeID,
+        shopDate: new Date(dateOfTransaction),
+        transactionId: transactionID,
     };
 }
 
-// Values from: https://gist.github.com/Protonk/5389384
-function lcg(seed) {
-    const m = 25;
-    const a = 11;
-    const c = 17;
-    // const m = Math.pow(2, 16) + 1;
-    // const a = 75;
-    // const c = 74;
-    
-    return (a * seed + c) % m;
-}
-
-function reverselcg(seed) {
-    const m = 25;
-    const a = 11;
-    const c = 17;
-    // const m = Math.pow(2, 16) + 1;
-    // const a = 75;
-    // const c = 74;
-
-    // const ainverse = gcdExtended(a, m, 0, 0);
-    const ainverse = egcd(a, m).y;
-
-    console.log(ainverse * (seed - c));
-
-    return ainverse * (seed - c) % m;
-}
-
-function testlcg() {
-    let seed = 2;
-    for(let i = 0; i < 10; i++) {
-        // console.log(lcg(seed));
-        seed = lcg(seed);
+// Not crypographically secure at all, but random enough to be usable.
+function deterministicReversibleRandom(seed, numDigits, maxValue) {
+    let valueInBinaryAsArray = seed.toString(2).split('').reverse();
+    if(valueInBinaryAsArray.length > numDigits) {
+        console.error("Seed value is larger than maximum number of binary digits");
+        return 0;
     }
 
-    console.log('split');
-
-    for(let i = 0; i < 10; i++) {
-        // console.log(reverselcg(seed));
-        seed = reverselcg(seed);
-    }
-}
-
-// From https://www.geeksforgeeks.org/euclidean-algorithms-basic-and-extended/
-function gcdExtended(a, b, x, y) {
-    // Base Case
-    if (a == 0) {
-        x = 0;
-        y = 1;
-        return b;
+    while(valueInBinaryAsArray.length < numDigits) {
+        valueInBinaryAsArray.push('0');
     }
 
-    // To store results of recursive call
-    let gcd = gcdExtended(b % a, a, x, y);
-
-    // Update x and y using results of recursive call
-    x = y - (b / a) * x;
-    y = x;
-
-    return {
-        "gcd": b,
-        "x": x,
-        "y": y
-    };
-}
-
-function egcd(a,b) {
-    if (a < b) [a,b] = [b, a];
-    let s = 0, old_s = 1;
-    let t = 1, old_t = 0;
-    let r = b, old_r = a;
-    while (r != 0) {
-        let q = Math.floor(old_r/r);
-        [r, old_r] = [old_r - q*r, r];
-        [s, old_s] = [old_s - q*s, s];
-        [t, old_t] = [old_t - q*t, t];
-    }
-    return {
-        "gcd": old_r,
-        "x": old_s,
-        "y": old_t
-    }
-}
-
-function testegcd() {
-    egcd(50, 35);
-}
-
-// https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
-
-// Sine is not a great source of randomness, but it functions well enough for this purpose.
-// This randomisation is mostly to obfuscate the codes in a way that can be reversed.
-function deterministicRandom(seed, minValue, maxValue) {
-
-    // 0 and all exact multiples of pi trend toward 0.
-    if(seed === 0 || !Number.isInteger(seed)) {
-        throw("Seed cannot be 0 or a non-integer");
-    }
-
-    const sinValue = Math.sin(seed) * 10000; // Multiplying here makes it drop more leading values, which helps with the pseudo-randomness
-    const randomValue = sinValue - Math.floor(sinValue);
-    return parseInt(Math.round((maxValue - minValue) * randomValue));
+    let percentage = (parseInt(valueInBinaryAsArray.join(''), 2))/(Math.pow(2, numDigits) - 1);
+    return Math.round(percentage * maxValue);
 }
 
 // Fisher–Yates shuffle
 function arrayRandomisation(array, seed) {
     const arrayCopy = JSON.parse(JSON.stringify(array)); // without a deep copy, it modifies the original array repeatedly
 
-    let currentIndex = array.length;
+    // let currentIndex = array.length;
     let randomIndex = 0;
 
-    while (currentIndex > 0) {
-        randomIndex = deterministicRandom(seed++, 0, currentIndex);
-        currentIndex--;
+    for(let i = array.length-1;i>=0;i--) {
+        randomIndex = deterministicReversibleRandom(seed++, 16, array.length-1);
 
-        value1 = arrayCopy[currentIndex];
+        value1 = arrayCopy[i];
         value2 = arrayCopy[randomIndex];
-        arrayCopy[currentIndex] = value2;
+        arrayCopy[i] = value2;
         arrayCopy[randomIndex] = value1;
     }
 
     return arrayCopy;
 }
 
-function convertToAnyBase(base, base10Value, lookupTable, finalLength) {
+// Fisher-Yates shuffle can be reversed by performing its steps in reverse.
+// Essentially just reverse the seed, since it generates sequentially.
+function arrayReverseRandomisation(shuffledArray, seed) {
+    const arrayCopy = JSON.parse(JSON.stringify(shuffledArray)); // without a deep copy, it modifies the original array repeatedly
+    endSeed = seed + shuffledArray.length-1;
+
+    // let currentIndex = array.length;
+    let randomIndex = 0;
+
+    for(let i = 0; i < shuffledArray.length; i++) {
+        randomIndex = deterministicReversibleRandom(endSeed--, 16, shuffledArray.length-1);
+
+        value1 = arrayCopy[i];
+        value2 = arrayCopy[randomIndex];
+        arrayCopy[i] = value2;
+        arrayCopy[randomIndex] = value1;
+    }
+
+    return arrayCopy;
+}
+
+function convertToAnyBase(base, base10Value, lookupTable, maxDigits) {
+    if(base10Value > Math.pow(base, maxDigits)) {
+        console.error(`${base10Value} is too large to fit into ${maxDigits} base ${base} digits.`);
+        return "";
+    }
+
+    if(lookupTable.length < base) {
+        console.error("Too few characters in lookup table");
+        return "";
+    }
+
     let quotient = base10Value;
     
     let base64String = "";
@@ -303,8 +200,8 @@ function convertToAnyBase(base, base10Value, lookupTable, finalLength) {
 
     base64String = lookupTable[quotient] + base64String;
 
-    // Adds leading zeroes if the string is too short
-    while(base64String.length < finalLength) {
+    // Adds leading zeroes if the string is too short. Ensures that string length is consistent.
+    while(base64String.length < maxDigits) {
         base64String = lookupTable[0] + base64String;
     }
 
@@ -325,9 +222,7 @@ function convertAnyToBase10(base, value, lookupTable) {
     return total;
 }
 
-function printRandom(seed, minValue, maxValue) {
-    console.log(deterministicRandom(seed, minValue, maxValue));
-}
+// Test buttons
 
 function printArray() {
     console.log(arrayRandomisation(characterLookupTable, 235235));
@@ -351,7 +246,8 @@ function testEncodeAndDecode2(storeID, transactionID) {
 
 function testEncode() {
     for(let i = 0; i < 200; i++) {
-        const sc = generateShortCode2(i, Math.floor(Math.random() * (10000 - 0 + 1) + 0));
+        const sc = generateShortCode(i, Math.floor(Math.random() * (10000 - 0 + 1) + 0));
+        // const sc = generateShortCode(1, i);
         console.log(sc);
     }
 }
@@ -366,4 +262,50 @@ function testBaseConverstion(base, value) {
     converted = convertToAnyBase(base, value, characterLookupTable, 10);
     console.log(converted);
     console.log(convertAnyToBase10(base, converted, characterLookupTable));
+}
+
+function testlcg() {
+    let seed = 2;
+    for(let i = 0; i < 10; i++) {
+        // console.log(lcg(seed));
+        seed = lcg(seed);
+    }
+
+    console.log('split');
+
+    for(let i = 0; i < 10; i++) {
+        // console.log(reverselcg(seed));
+        seed = reverselcg(seed);
+    }
+}
+
+function testRev(value) {
+    for(let i = 0;i < 200; i++) {
+        console.log(rev(i));
+    }
+}
+
+function testArrayRandom() {
+    for(let i = 1;i<1000;i++) {
+        console.log(arrayRandomisation(characterLookupTable, i));
+    }
+}
+
+function testReversibleRandom() {
+    let seed = 2141;
+
+    for(let i = seed; i < seed + 10; i++) {
+        console.log(deterministicReversibleRandom(i, 16, 64));
+    }
+
+    for(let i = seed + 10; i >= seed; i--) {
+        console.log(deterministicReversibleRandom(i, 16, 64));
+    }
+    // console.log(deterministicReversibleRandom(Math.floor(Math.random() * (10000 - 0 + 1) + 0), 16, 64));
+}
+
+function testArrayUnrandomisation() {
+    let shuffledArray = arrayRandomisation(characterLookupTable, 141);
+    console.log(shuffledArray);
+    console.log(arrayReverseRandomisation(shuffledArray, 141))
 }
